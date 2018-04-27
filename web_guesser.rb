b73a0443f2bb3e412e8457caeccd8800da793261
Eric Miller <ericmailr@gmail.com>
2018-04-27 09:42:02 -0600

    add changing background color

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,22 +3,26 @@ require 'sinatra/reloader'
 
 SECRET_NUMBER = rand(101)
 get '/' do
-	guess = params["guess"].to_i
-	message = check_guess(guess)
-	erb :index, :locals => {:message => message}
+	guess = params["guess"]
+	guess_result = check_guess(guess)
+	message = guess_result[0]
+	color = guess_result[1]
+	erb :index, :locals => {:color => color, :message => message}
 end
 
 def check_guess(guess)
 	ret = ""
-	if guess > SECRET_NUMBER + 5
-		ret << "Way too high!"
-	elsif guess > SECRET_NUMBER
-		ret << "Too high!"
-	elsif guess < SECRET_NUMBER - 5
-		ret << "Way too low!"
-	elsif guess < SECRET_NUMBER
-		ret << "Too low!"
+	if guess == nil
+		return [ret, 'white']
+	elsif guess.to_i > SECRET_NUMBER + 5
+		return [ "Way too high!", 'red' ]
+	elsif guess.to_i > SECRET_NUMBER
+		return [ "Too high!", 'darksalmon' ]
+	elsif guess.to_i < SECRET_NUMBER - 5
+		return [ "Way too low!", 'red' ]
+	elsif guess.to_i < SECRET_NUMBER
+		return [ "Too low!", 'darksalmon' ]
 	else
-		ret << "Nailed it!\n\nThe SECRET NUMBER was #{SECRET_NUMBER}."
+		return [ "Nailed it!\n\nThe SECRET NUMBER was #{SECRET_NUMBER}.", 'green' ]
 	end	
 end
